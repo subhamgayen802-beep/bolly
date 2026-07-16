c25f9450574546bada6f339ce5591908b8a56651
Subham Gayen <subhamgayen802@gmail.com>
2026-07-17 00:11:26 +0530

    fix main

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import {store} from './store/store.js'
 import { Provider } from 'react-redux'
-import { CartProvider } from './context/CartContext';
+import { CartProvider } from './context/cartContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   
